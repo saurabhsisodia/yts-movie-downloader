cd34fd4d902f0e8b33ddabc9174b05d9d7cca2df
Saurabh Sisodia <ssisodia507@gmail.com>
2021-06-26 23:11:40 +0530

    Update settings.py

--- yts_recent_movies/yts_recent_movies/settings.py
+++ yts_recent_movies/yts_recent_movies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=4-$-xf646#g!(l)3-&amb1-*mu149uufyb*hwg%jn9!(nwlku'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
